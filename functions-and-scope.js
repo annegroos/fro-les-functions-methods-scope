27795b7a83ec3f1681f87d57bca9d6c6435b42d7
annegroos <anne-groos@hotmail.com>
2026-01-20 15:51:02 +0100

    geprobeerd de opdrachten van functions-and-scopes te maken, maar ik kom er echt niet uit

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -4,6 +4,9 @@
 
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
+// ------------------ HET IS ME NIET GELUKT. KUNNEN JULLIE ME DIT UITLEGGEN? -----------------//
+
+
 /* Opdracht  1: Cum Laude */
 
 /* 1a: Script schrijven  */
@@ -15,6 +18,22 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
+console.log("opdracht 1a")
+// 1. for-loop
+// 2. .length methode
+// 3. const?
+// for (let i = 0; i < grades.length; i++) {
+//     const cumlaude = grades[i] <=8;
+//         console.log(cumlaude);
+// }
+//  >>>> Dit geeft a boolean uitkomst, dus dit klopt niet <<<< //
+
+// let totalCumlaude = 0;
+// for (let i = 0; i < grades.length; i++) {
+//     if (grades[i] <=8) {
+//         console.log(totalCumlaude += i);
+//     }
+// }
 
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
@@ -27,9 +46,10 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+ // ----------------- IK RAAK IN DE WAR MET PYTHON NU. IK KOM ER NIET UIT ----------------- //
 
 
-
+// =======================================================================================================================
 /* Opdracht  2: Gemiddeld cijfer */
 
 /* 2a: Script schrijven  */
@@ -42,6 +62,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
+// 1. gemiddelde is getal/2
+// 2. alle cijfers bij elkaar optellen en dat totaal / 2
+// 3. for-loop + .length methode
+
+console.log("opdracht 2a");
+let totalGrade = 0;
+for (let i = 0; i < grades.length; i++) {
+    console.log((totalGrade += i)/2)
+}
+
+ // ------------------ OOK HIER RAAK IK IN DE WAR. IK KOM ER NIET UIT ---------------- //
 
 /* 2b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
